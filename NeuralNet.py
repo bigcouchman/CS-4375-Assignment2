e8b6f2cf1a4bdc4b72724509b8059659ccb01735
Nguyen P. Do <dophucnguyen2005@gmail.com>
2026-03-04 15:49:02 -0600

    Added comments

--- NeuralNet.py
+++ NeuralNet.py
@@ -8,7 +8,7 @@
 #
 #####################################################################################################################
 
-
+# Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,6 +22,7 @@ from ucimlrepo import fetch_ucirepo
 
 class NeuralNet:
     def __init__(self, d_id = 186):
+        # Get Red and White wine data from UCI dataset, get features and target
         dataset = fetch_ucirepo(id = d_id)
         X = dataset.data.features
         y = dataset.data.targets
@@ -33,11 +34,13 @@ class NeuralNet:
     # standardization, normalization,
     #   categorical to numerical, etc
     def preprocess(self):
+        # Handle null and duplicates, helping on stability
         dframe = self.raw_input.fillna(self.raw_input.mean())
         dframe.drop_duplicates(inplace=True)
         X = dframe.drop(self.target_name, axis=1)
         y = dframe[self.target_name]
 
+        # Standardizing data by putting it to the same scale
         self.X_processed = (X - X.mean()) / X.std()
         self.y_processed = y
 
@@ -52,6 +55,7 @@ class NeuralNet:
     #       different color for each model
 
     def train_evaluate(self):
+        # Train test split (80/20), create directories to save logs and graphs
         X_train, X_test, y_train, y_test = train_test_split(self.X_processed, self.y_processed, test_size = 0.2, random_state = 42)
         os.makedirs("plots", exist_ok=True)
         os.makedirs("logs", exist_ok=True)
@@ -64,25 +68,28 @@ class NeuralNet:
         num_hidden_layers = [2, 3]
         alphas = [0.0001, 0.1]
 
-        # Create the neural network and be sure to keep track of the performance
-        #   metrics
-
+        # Store metrics in result array and create figure grid for graphs
         results = []
         fig, axes = plt.subplots(1, 3, figsize=(20,6), sharey=True)
+
+        # Fine tune parameters
         for element, i in enumerate(activations):
             ax = axes[element]
             for j in learning_rate:
                 for k in max_iterations:
                     for l in num_hidden_layers:
                         for alp in alphas:
+
+                            # Set a low number of neurons to avoid overfitting
                             hidden_layers = tuple([12] * l)
                             mlpClass = MLPClassifier(hidden_layer_sizes=hidden_layers, activation=i, learning_rate_init=j, max_iter=k, alpha = alp, random_state=1)
-                        
 
+                            # Suppress warning
                             with warnings.catch_warnings():
                                 warnings.filterwarnings("ignore", category=ConvergenceWarning)
                                 mlpClass.fit(X_train, y_train)
 
+                            # Calculate metrics for performance evaluation
                             predict_train = mlpClass.predict(X_train)
                             predict_test = mlpClass.predict(X_test)
 
@@ -101,6 +108,8 @@ class NeuralNet:
                                 "Training MSE": round(mse_train, 4),
                                 "Test MSE": round(mse_test, 4),
                             })
+
+                            # Plotting loss over time
                             ax.plot(mlpClass.loss_curve_, label=title)
 
             # Plot the model history for each model in a single plot
@@ -115,10 +124,10 @@ class NeuralNet:
         
         plt.suptitle("Neural Network Training History", fontsize=16)
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.show()
+        
 
         
-        
+        # Gather metrics via a dataframe to display in log
         dframe_results = pd.DataFrame(results)
         accur_avg_train = dframe_results.groupby('Activation')['Training Accuracy'].mean()
         accur_avg_test = dframe_results.groupby('Activation')['Test Accuracy'].mean()
@@ -127,11 +136,13 @@ class NeuralNet:
         best_test = dframe_results.loc[dframe_results['Test Accuracy'].idxmax()]
         worst_test = dframe_results.loc[dframe_results['Test Accuracy'].idxmin()]
         
+        # Store graphs and logs
         plotting = os.path.join("plots", "nn_training_history.png")
         plt.savefig(plotting)
         logging = os.path.join("logs", "results.csv")
         dframe_results.to_csv(logging, index=False)
 
+        # Print the results
         print("\n--- Result table ---")
         print(dframe_results.to_string(index=False))
 
@@ -145,6 +156,7 @@ class NeuralNet:
         print(f"Best Accuracy: {best_train['Label']} | Accuracy: {best_train['Training Accuracy']}")
         print(f"Worst Accuracy: {worst_train['Label']} | Accuracy: {worst_train['Training Accuracy']}")
 
+# Main to call neural network funcitons
 if __name__ == "__main__":
     neural_network = NeuralNet(186) # put in path to your file
     neural_network.preprocess()
